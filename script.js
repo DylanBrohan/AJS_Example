51de78c012a833476fe2e47f3daeb5b89a384d5a
Dylan <dylan@Dylans-MacBook-Pro.local>
2018-10-04 13:06:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// const square = function(x){
+//     return x * x;
+//     console.log(x);
+// };
+
+
+const square =  x => x*x; 
+
+console.log(square(6));
+
+
+const logDateTime = function(){
+    console.log(new Date());
+};
+
+// const logDateTime = () => //with no parameter () come in 
+// console.log(new Date());
